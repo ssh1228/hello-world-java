9842426173d783d461044d227a011246999e2e22
EC2 Default User <ec2-user@ip-172-31-63-177.ec2.internal>
2021-01-01 00:34:28 +0000

    adding Helloworld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,6 +36,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     
-    System.out.println("Hello, devops");
+   System.out.println("Hello, devops");
+   System.out.println("Hello, sandy");
+   System.out.println("Hello, candys");
   }
 }
